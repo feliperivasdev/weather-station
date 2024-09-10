4b4a1333056346e4be16d4364e64d47f54e937f7
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-09-09 21:22:41 -0500

    Create database.py

---
+++ API/database.py
@@ -0,0 +1,12 @@
+'''
+Dev : Felipe Rivas
+Script description: weather station Database
+Engine: SQLite3
+Version: 1.0
+Date: 9/8/2024
+'''
+# Importing libraries database engine
+import sqlite3
+
+#Creating weather station database connection
+conn = sqlite3.connect('weather_station.db')
