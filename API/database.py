2cf6a49d428451d1b8c6a7e47f245a939651afa9
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-09-09 21:49:39 -0500

    Update database.py

--- API/database.py
+++ API/database.py
@@ -11,5 +11,25 @@ import sqlite3
 #Creating weather station database connection
 conn = sqlite3.connect('weather_station.db')
 
+#Create cursor
+cur = conn.cursor()
+
+#Create model users
+users_model = '''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    status BOOLEAN DEFAULT true,
+    created_at TIMESTAMP DEFAULT (datetime('now','localtime')),
+    updated_at TIMESTAMP DEFAULT (datetime('now','localtime')),
+    deleted_at TIMESTAMP DEFAULT NULL
+)
+'''
+#Execute query
+cur.execute(users_model)
+
 #Close connection
 conn.close()
